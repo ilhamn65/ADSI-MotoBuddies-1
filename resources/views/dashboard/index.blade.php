4726d6be800c4efedce7822842ab8515c4f65914
Arrafii Alfa <arrafiialfa.m@gmail.com>
2022-05-02 02:39:45 +0700

    tailwind configured

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,8 +1,6 @@
-
-
 @extends('layouts.app')
 
-
 @section('content')
-asdasdasd
+
+   
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Motobuddy<title>
-
+    <title>Motobuddies</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
-
-<body>
+<body class="bg-blue-400">
+    <nav class="flex justify-between bg-white p-6 ">
+        
+        <ul class="flex items-center"><li>
+            <a class="p-3">Home</a>
+        </li>
+        <li>
+            <a class="p-3">Dashboard</a>
+        </li>
+        <li>
+            <a class="p-3">Our Products</a>
+        </li>
+        <li>
+            <a class="p-3">Our Services</a>
+        </li></ul>
+        <ul class="flex items-center"><li>
+            <a class="p-3">Arrafii Alfa</a>
+        </li>
+        <li>
+            <a class="p-3">Your Orders</a>
+        </li>
+        <li>
+            <a class="p-3">Contact Support</a>
+        </li>
+        <li>
+            <a class="p-3">Logout</a>
+        </li></ul>
+        
+       
+           
+                
+            
+    </nav>
     @yield('content')
-    
 </body>
-
 </html>
