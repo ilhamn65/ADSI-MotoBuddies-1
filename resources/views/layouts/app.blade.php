a0c8216350ef2d9c6140c0598ce721ea2cc0efd0
Arrafii Alfa <arrafiialfa.m@gmail.com>
2022-05-02 00:41:32 +0700

    tailwindcss install

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Motobuddy<title>
+
+</head>
+
+<body>
+
+    <h1>Motobuddy</h1>
+</body>
+
+</html>
