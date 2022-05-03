ed841155d1e182cb012612d6d6972387f6bf72b3
Arrafii Alfa <arrafiialfa.m@gmail.com>
2022-05-03 18:22:31 +0700

    added top and bottom navigation without img

--- storage/framework/views/476cacff28d0c2060708eb1ed714663575f9bb53.php
+++ storage/framework/views/476cacff28d0c2060708eb1ed714663575f9bb53.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,39 +8,71 @@
     <title>Motobuddies</title>
     <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
 </head>
-<body class="bg-blue-400">
-    <nav class="flex justify-between bg-white p-6 ">
-        
-        <ul class="flex items-center"><li>
-            <a class="p-3">Home</a>
-        </li>
-        <li>
-            <a class="p-3">Dashboard</a>
-        </li>
-        <li>
-            <a class="p-3">Our Products</a>
-        </li>
-        <li>
-            <a class="p-3">Our Services</a>
-        </li></ul>
-        <ul class="flex items-center"><li>
-            <a class="p-3">Arrafii Alfa</a>
-        </li>
-        <li>
-            <a class="p-3">Your Orders</a>
-        </li>
-        <li>
-            <a class="p-3">Contact Support</a>
-        </li>
-        <li>
-            <a class="p-3">Logout</a>
-        </li></ul>
-        
-       
-           
-                
-            
-    </nav>
+
+<body class="bg-gray-200">
+    <section id="top-navigation" class="fixed inset-x-0 z-10 bg-blue-400 p-6 text-white  rounded-b-2xl ">
+        <div id="notification" class="flex justify-end gap-2">
+
+            <a>
+                <span>Cart</span>
+            </a>
+            <a>
+                <span>Notif</span>
+            </a>
+            <a>
+                <span>Mssge</span>
+            </a>
+
+        </div>
+
+        <div id="search-bar">
+            <div>Selamat Datang, Justin !</div>
+            <form action="">
+
+                <input class="text-gray-400 py-1 px-3 w-full" type="text" name="search" value="Cari Sesuatu ?">
+            </form>
+        </div>
+
+
+
+    </section>
     <?php echo $__env->yieldContent('content'); ?>
+
+    <!-- component -->
+    <div class="w-full h-screen">
+
+        <nav id="bottom-navigation" class="p-6 block fixed inset-x-0 bottom-0 z-10 bg-white shadow">
+            <div id="tabs" class="flex justify-between">
+                <a href="#"
+                    class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
+                    
+                    <span class="tab tab-home block text-xs">Beranda</span>
+                </a>
+                <a href="#"
+                    class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
+
+                    <span class="tab tab-kategori block text-xs">History</span>
+                </a>
+                <a href="#"
+                    class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
+
+                    <span class="tab tab-explore block text-xs">Emergency</span>
+                </a>
+                <a href="#"
+                    class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
+
+                    <span class="tab tab-whishlist block text-xs">Contact Us</span>
+                </a>
+                <a href=#
+                    class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
+
+                    <span class="tab tab-account block text-xs">Profile</span>
+                </a>
+            </div>
+        </nav>
+    </div>
+
 </body>
-</html><?php /**PATH D:\Web development\motobuddies-app\resources\views/layouts/app.blade.php ENDPATH**/ ?>
+
+</html>
+<?php /**PATH D:\Web development\motobuddies-app\resources\views/layouts/app.blade.php ENDPATH**/ ?>
