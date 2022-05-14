c968b19656a6433f762fe1881e61d8d36296703d
ilhamn65 <ilhamnuansa65@gmail.com>
2022-05-14 13:03:28 +0700

    Revert "update test view FAQ"
    This reverts commit 9ef0db3e792875a38c6dfa18795ea8dfde6b490b.

--- resources/views/FAQ/index.blade.php
+++ resources/views/FAQ/index.blade.php
@@ -12,25 +12,23 @@
             text-align: left;
             outline: none;
             font-size: 15px;
-            font-weight: bold;
-            
+
             filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
         }
 
         .active,
         .collapsible:hover {
-            background-color: #C7D4DF;
-            color: #474747;
+            background-color: #474747;
+            color: white;
         }
 
         .content {
             padding: 0 18px;
             display: none;
             overflow: hidden;
-            background-color: #C7D4DF;
-            color: #474747;
+            background-color: #f1f1f1;
         }
-        /* color white f1f1f1 */
+
     </style>
 
     <button type="button" class="collapsible">Apakah pembelian produk dapat dikembalikan?</button>
@@ -56,18 +54,6 @@
             ea commodo consequat.</p>
     </div>
     <button type="button" class="collapsible">Bisakah saya menerima pengembalian uang?</button>
-    <div class="content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.</p>
-    </div>
-    <button type="button" class="collapsible">Lorem ipsum dolor sit amet, consectetur?</button>
-    <div class="content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.</p>
-    </div>
-    <button type="button" class="collapsible">Lorem ipsum dolor sit amet, consectetur?</button>
     <div class="content">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
             dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
