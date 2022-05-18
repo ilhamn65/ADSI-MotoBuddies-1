cd1fb6fa5061276624cdc4a720a2dd900f1622c0
Arrafii Alfa <arrafiialfa.m@gmail.com>
2022-05-18 19:33:52 +0700

    change column

--- resources/views/mybookings/index.blade.php
+++ resources/views/mybookings/index.blade.php
@@ -7,8 +7,10 @@
                 <tr>
 
                     <th>Kendaraan</th>
-                    <th>Plat Nomor</th>
+
                     <th>Waktu Booking</th>
+                    <th>Estimasi Harga</th>
+
                 </tr>
             </thead>
             <tbody class="bg-gray-50 text-center">
@@ -17,8 +19,8 @@
                     <tr class="border-2 border-gray-200">
 
                         <td class="px-2 py-1">{{ $booking->merek_kendaraan }} {{ $booking->model_kendaraan }}</td>
-                        <td class="px-2 py-1">{{ $booking->plat_nomor_kendaraan }} </td>
                         <td class="px-2 py-1 ">{{ $booking->waktu_mulai }}</td>
+                        <td class="px-2 py-1 ">Rp. {{ $booking->perkiraan_harga }}</td>
                     </tr>
                 @endforeach
 
